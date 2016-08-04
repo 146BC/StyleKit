cef3c9122b8a2b15fd91258bc34e8f1a15e67106
Bernard Gatt <bernard@blinkrocket.com>
2016-08-05 00:33:12 +0200

    Initial Commit

---
+++ StyleKit/StyleKit.h
@@ -0,0 +1,19 @@
+//
+//  StyleKit.h
+//  StyleKit
+//
+//  Created by Bernard Gatt on 05/08/2016.
+//  Copyright © 2016 Bernard Gatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StyleKit.
+FOUNDATION_EXPORT double StyleKitVersionNumber;
+
+//! Project version string for StyleKit.
+FOUNDATION_EXPORT const unsigned char StyleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyleKit/PublicHeader.h>
+
+
