67217b1a22d0f29cf2ef776e39e7b0d73536add6
Grant Kemp <grant@kempnet.com>
2016-09-01 08:35:34 +0100

    feature: added StylePreview VC

---
+++ StyleKitDemo/StyleKitDemo/StylePreviewerViewController.swift
@@ -0,0 +1,43 @@
+//
+//  StylePreviewerViewController.swift
+//  StyleKitDemo
+//
+//  Created by Grant Kemp on 01/09/2016.
+//  Copyright © 2016 Bernard Gatt. All rights reserved.
+//
+
+import UIKit
+
+class StylePreviewerViewController: UIViewController {
+
+    @IBOutlet weak var tf_SelectFont: UITextField!
+    
+    @IBOutlet weak var picker_FontSelector: UIPickerView!
+    
+    @IBOutlet weak var lbl_textPreview: UILabel!
+    
+    //MARK: UI Actions
+    
+    @IBAction func atf_selectedFontTextField(sender: UITextField) {
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    //MARK: Methods
+    
+    func _StartSelectingFont() {
+        
+    }
+    
+    func _DidSelectFontfromPicker() {
+        
+    }
+    
+}
