d9e2dce7ec007d5d2d7da44f0af3f52500cf1fe8
Jakub Petrík <petrik@inloop.eu>
2017-11-07 14:19:43 +0100

    Add attributes UIAppearance accessor

--- StyleKitDemo/StyleKitDemo/CustomViews/SKLabel.swift
+++ StyleKitDemo/StyleKitDemo/CustomViews/SKLabel.swift
@@ -4,10 +4,26 @@ import UIKit
 class SKLabel: UILabel {
     
     @objc dynamic var padding: [CGFloat] = [0,0,0,0]
-    
+    @objc dynamic var attributes: [NSAttributedStringKey: Any]? {
+        didSet {
+            updateAttributedText()
+        }
+    }
+
+    override var text: String? {
+        didSet {
+            updateAttributedText()
+        }
+    }
+
+    func updateAttributedText() {
+        guard let string = attributedText?.string
+            , let attributes = attributes else { return }
+        attributedText = NSAttributedString(string: string, attributes: attributes)
+    }
+
     override func drawText(in rect: CGRect) {
-        let insets = UIEdgeInsets.init(top: padding[0], left: padding[1], bottom: padding[2], right: padding[3])
+        let insets = UIEdgeInsets(top: padding[0], left: padding[1], bottom: padding[2], right: padding[3])
         super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
     }
-    
 }
